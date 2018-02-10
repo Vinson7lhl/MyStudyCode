c916ed6310a3a12e22c9c6652bcc692cc615de1a
Seven Li-后会有七 <vinson7lhl@126.com>
2018-02-10 19:34:14 +0800

    modify some string
    1、change [double quotation mark] into [quotation mark].
    2、remove uselsee configuration.

--- angular4Test/src/app/app.module.ts
+++ angular4Test/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeroService } from './service/hero.service';
-import { AuthService } from "./service/auth.service";
-import { LoggedInGuard } from "./service/loggedIn.guard.service";
+import { AuthService } from './service/auth.service';
+import { LoggedInGuard } from './service/loggedIn.guard.service';
 
-import { RouterModule }   from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './components/hello-world/hello-world.component';
@@ -15,13 +15,13 @@ import { UserListComponent } from './components/user-list/user-list.component';
 import { TestComComponent } from './components/test-com/test-com.component';
 import { HerosComponent } from './components/heros/heros.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
-import { ProtectedComponent } from "./components/protected/protected.component";
+import { ProtectedComponent } from './components/protected/protected.component';
 
-//路由配置Routes
+// 路由配置Routes
 import { HEROROUTER } from './router/hero-router';
 import { MyDeirectiveComponent } from './my-deirective/my-deirective.component';
 
-//material
+// material
 import {
   MatAutocompleteModule,
   MatButtonModule,
@@ -55,10 +55,10 @@ import {
   MatTooltipModule,
   MatStepperModule,
 } from '@angular/material';
-import {CdkTableModule} from '@angular/cdk/table';
+import { CdkTableModule } from '@angular/cdk/table';
 
 @NgModule({
-  //组件
+  // 组件
   declarations: [
     AppComponent,
     HelloWorldComponent,
@@ -70,13 +70,13 @@ import {CdkTableModule} from '@angular/cdk/table';
     MyDeirectiveComponent,
     ProtectedComponent
   ],
-  //模块
+  // 模块
   imports: [
     BrowserModule,
-    //formsModule作用是导入表单元素的指令，如[(ngModel)]，否则在表单中会报错说ngModel无法识别
+    // formsModule作用是导入表单元素的指令，如[(ngModel)]，否则在表单中会报错说ngModel无法识别
     FormsModule,
     ReactiveFormsModule,
-    //路由
+    // 路由
     RouterModule.forRoot(HEROROUTER),
     BrowserAnimationsModule,
     CdkTableModule,
@@ -112,8 +112,8 @@ import {CdkTableModule} from '@angular/cdk/table';
     MatToolbarModule,
     MatTooltipModule
   ],
-  //服务、请求
-  providers: [HeroService,AuthService,LoggedInGuard],
+  // 服务、请求
+  providers: [HeroService, AuthService, LoggedInGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
