d2ab4d76486b293c1fa568fd60e3bb3a64cd0747
Seven Li <seven.h.li@pwc.com>
2018-04-10 09:39:59 +0800

    react and css3-flex

--- angular4Test/src/app/router/hero-router.ts
+++ angular4Test/src/app/router/hero-router.ts
@@ -1,6 +1,6 @@
 //Routes就是一种数据的类型的结构，如string，number一样，所以导出的数组必须满足Routes的结构才行
 import { Routes } from '@angular/router';
-import { HerosComponent,childRouter } from '../components/heros/heros.component';
+import { HerosComponent, childRouter } from '../components/heros/heros.component';
 import { ProtectedComponent } from '../components/protected/protected.component';
 import { AuthService } from "../service/auth.service";
 import { LoggedInGuard } from "../service/loggedIn.guard.service";
@@ -9,20 +9,20 @@ import { LoggedInGuard } from "../service/loggedIn.guard.service";
  * prefix：其实是阶段匹配，即有子路由，如果path=abc, 而路径是abc/xxx,是可以重定向的，而full则不可以，原因是full要求path必须完全一样才行
  * full:必须是完整匹配path才能够重定向，如下例子：path必须且只能是“”才能重定向
  */
-export const HEROROUTER:Routes=[
+export const HEROROUTER: Routes = [
   {
     path: '',
-    pathMatch:'full',
-    redirectTo:'/heros'
+    pathMatch: 'full',
+    redirectTo: '/heros'
   },
   {
     path: 'heros',
     component: HerosComponent,
-    children:childRouter
+    children: childRouter
   },
   {
     path: 'protected',
     component: ProtectedComponent,
-    canActivate:[LoggedInGuard]
+    canActivate: [LoggedInGuard]
   }
 ];
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+/**
+ * 从这里开始引导根组件APP
+ */
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
