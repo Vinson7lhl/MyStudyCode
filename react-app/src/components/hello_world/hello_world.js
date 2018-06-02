6e94ecb9e389c1dcb76d7bb1fe981bbe9a5d15b8
Seven Li-后会有七 <vinson7lhl@126.com>
2018-06-02 21:42:49 +0800

    react componentDidMount

--- react-app/src/components/hello_world/hello_world.js
+++ react-app/src/components/hello_world/hello_world.js
@@ -9,14 +9,32 @@ function FunCom(props){
 }
 
 class Hello extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state={
+            name:'欧阳锋',
+            age:62,
+            location:'西北',
+            date:new Date(),
+        };
         console.log('Hello构造器');
     }
+
+    componentDidMount() {
+        console.log('生命周期：componentDidMount，组件被输出渲染后运行，可放异步请求');
+    }
+  
+    componentWillUnmount() {
+        console.log('生命周期：componentWillUnmount');
+    }
+
     render() {
         return (
             <div className="DiyHello">
-                hello world! 李宏磊；
+                <div>姓名：{this.state.name}</div>
+                <div>年龄{this.state.age}</div>
+                <div>地理位置：{this.state.location}</div>
+                <div>日期：{this.state.date.toLocaleTimeString()}</div>
         </div>
         );
     }
