4e9da3ed806f16440c4f213082f72fd34afabb4d
Seven Li-后会有七 <vinson7lhl@126.com>
2018-10-15 14:09:20 +0800

    提交

--- CSS3/js/css3.js
+++ CSS3/js/css3.js
@@ -9,7 +9,7 @@
 var wrongObj={
     'name':'lhl',
     age:20,
-    'name':'jj'
+    // 'name':'jj'
 }
 $(function()
 {
@@ -26,4 +26,15 @@ $(function()
     console.log("window.outerWidth-高级，窗口："+window.outerWidth);
     console.log("document.body.clientWidth-低级，视口："+document.body.clientWidth);
     console.log("document.documentElement.clientWidth-低级，窗口："+document.documentElement.clientWidth);
+
+    $(".content_2").mCustomScrollbar({
+
+        scrollInertia:150
+
+    });
+    $("body").mCustomScrollbar({
+
+        scrollInertia:150
+
+    });
 });
--- nextJs/.next/static/runtime/webpack.js
+++ nextJs/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fe1300b44efd892f95a8";
+/******/ 	var hotCurrentHash = "1345fe452642f97e4f76";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
