f05a9346cd2103db80551946987fb3ab93d26757
Seven Li-后会有七 <vinson7lhl@126.com>
2018-11-07 21:22:24 +0800

    提交

--- nextJs/.next/static/runtime/webpack.js
+++ nextJs/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8bb99062ae5e1cf37035";
+/******/ 	var hotCurrentHash = "6318c76d50b38f54d2a7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- nextJs/pages/index.js
+++ nextJs/pages/index.js
@@ -1,10 +1,27 @@
+
+import React, { Component } from 'react'
+
 import Header from '../comp/header'
 
-const Index = () => (
-    <div>
-      <Header/>
-      <p>Hello Next.js</p>
-    </div>
-  )
-  
-  export default Index
+export default class Index extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <p>Hello Next.js</p>
+      </div>
+    )
+  }
+}
+// const Index = () => (
+//     <div>
+//       <Header/>
+//       <p>Hello Next.js</p>
+//     </div>
+//   )
+
+//   export default Index
