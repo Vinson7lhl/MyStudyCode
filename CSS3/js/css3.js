bb0f4bc08ac254417756e874fca721eaa2df981f
honglei.li <honglei.li@molbase.com>
2019-01-04 20:05:19 +0800

    算法

--- CSS3/js/css3.js
+++ CSS3/js/css3.js
@@ -37,4 +37,48 @@ $(function()
         scrollInertia:150
 
     });
+
+
+
+    Date.prototype.getFullWeek = function(){
+        var jan1, w, d = new Date(this);
+        d.setDate(d.getDate()+4-(d.getDay()||7));		// Set to nearest Thursday: current date + 4 - current day number, make Sunday's day number 7
+        jan1 = new Date(d.getFullYear(),0,1);		// Get first day of year
+        w = Math.ceil((((d-jan1)/86400000)+1)/7);		// Calculate full weeks to nearest Thursday
+        return {y: d.getFullYear(), w: w };
+    };
+    //Returns ISO 8601 week number
+    Date.prototype.getWeek = function(){ 
+        return this.getFullWeek().w; 
+    };
+    var getWeeksInYear = function(y){ 
+        return new Date(y,11,28).getFullWeek().w; 
+    };
+    var test_array = ['6','7','1','2','3','4','5','6','7','1'];
+    var point_array = [1,8];
+    // for (var i=0;i<test_array.length;i++) {
+    //     if (test_array[i]==='7') {
+    //         point_array.push(i)//[0,2,9]
+    //         temp
+    //         start_time = i;
+    //         console.log(test_array[0]+'-'+test_array[i])
+    //     }
+    // }
+    for (var i=0;i<point_array.length;i++){
+        if (point_array[i]===0) {
+            console.log(test_array[point_array[i]]+'-'+test_array[point_array[i]]);
+        } else {
+            if (point_array[i]+1 === test_array.length){
+                console.log(test_array[point_array[i]+1]+'-'+test_array[point_array[i]+1]);
+            } else {
+                if (point_array[i+1]) {
+                    console.log(test_array[point_array[i]+1]+'-'+test_array[point_array[i+1]]);
+                } else {
+                    console.log(test_array[point_array[i]+1]+'-'+test_array[test_array.length-1]);
+                }
+                
+            }
+        }
+        
+    }
 });
