565e51a2552cd05ef37835503de1bedce1d0820f
honglei.li <honglei.li@molbase.com>
2019-03-15 15:19:29 +0800

    提交
    增加传递的数据属性property

--- wechatApp/components/productBlock/index.js
+++ wechatApp/components/productBlock/index.js
@@ -2,13 +2,13 @@ Component({
     properties: {
         // 这里定义了innerText属性，属性值可以在组件使用时指定
         innerText: {
-            type: String,
-            value: 'default value',
+            type: Array,
+            value: [],
         }
     },
     data: {
         // 这里是一些组件内部数据
-        someData: {}
+        dataList: []
     },
     methods: {
         // 这里是一个自定义方法
