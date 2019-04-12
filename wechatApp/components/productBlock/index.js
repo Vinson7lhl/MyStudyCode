0d5c0360ccc2cb14719d076b83658c621b54ce33
honglei.li <honglei.li@molbase.com>
2019-04-12 14:21:53 +0800

    提交
    增加组件内部的事件

--- wechatApp/components/productBlock/index.js
+++ wechatApp/components/productBlock/index.js
@@ -1,31 +1,56 @@
 Component({
+    // 数据源之一
     properties: {
-        // 这里定义了innerText属性，属性值可以在组件使用时指定
+        // 这里定义了innerText属性，用于父向子组件传递数据
         innerText: {
             type: Array,
             value: [],
         }
     },
+    // 数据源之二
     data: {
         // 这里是一些组件内部数据
         dataList: []
     },
+    // 数据源之三
+    observers: {
+
+    },
+    // 生命周期
     lifetimes: {
         created  () {
-            console.log('组件被创建好')
+            console.log('created-组件被创建好')
         },
         attached () {
-            console.log('组件刚刚进入页面节点树');
+            console.log('attached-组件刚刚进入页面节点树');
         },
         detached () {
-            console.log('移除组件后');
+            console.log('detached-移除组件后');
         },
     },
+
+    ready() {
+        console.log('组件ready')
+    },
+    // 类似于mixins和traits的组件间代码复用机制
+    behaviors: [],
     methods: {
         // 这里是一个自定义方法
         customMethod() { },
         throwDataToFater() {
             this.triggerEvent('fatherEvent',{'forFather':'苹果'});
+        },
+        jumpToDetail(event) {
+            console.log(event)
+            wx.navigateTo({
+                url: '/pages/listDetail/listDetail',
+                success: (result) => {
+                    
+                },
+                fail: () => {},
+                complete: () => {}
+            });
+              
         }
     }
 })
---
+++ wechatApp/pages/listDetail/listDetail.js
@@ -0,0 +1,49 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  // 初始元数据
+  data: {
+  },
+  /**
+   * ----------------------------------------------------钩子函数列表---------------------------------------------------------------
+   */
+  onLoad: function () {
+    console.log('page/listDetail：onLoad，页面加载完，通过路由进入此页面每次都会触发一次，')
+  },
+  // 此钩子函数适合放异步请求，每次进入此页面都要冲洗初始化一次
+  onShow: function () {
+    console.log('page/listDetail：onShow,页面显示完，通过路由进入此页面每次都会触发一次/每次从后台切回此页面');
+    console.log('当前的路径是：'+this.route)
+  },
+  onReady: function () {
+    console.log('page/listDetail：onReady,页面渲染完');
+  },
+  onHide: function () {
+    console.log('page/listDetail：onHide,页面隐藏/切换到后台触发一次');
+  },
+  onUnload: function () {
+    console.log('page/listDetail：onUnload,页面卸载(后退触发)');
+  },
+  /**
+   * ----------------------------------------------------事件触发列表---------------------------------------------------------------
+   */
+  onPullDownRefresh () {
+    console.log('onPullDownRefresh，用户在下拉……')
+  },
+  onReachBottom () {
+    console.log('onReachBottom，用户在上拉触底……')
+  },
+  onPageScroll () {
+    console.log('onPageScroll，用户在滚动……')
+  },
+  onResize () {
+    console.log('onResize，页面尺寸改变时触发……')
+  },
+  onTabItemTap () {
+    console.log('onTabItemTap，切换tab时触发……')
+  },
+  /**
+   * ----------------------------------------------------自定义事件---------------------------------------------------------------
+   */
+})
