1f2ebc481da1e1bced31ec2016e1d8f5ece3e760
honglei.li <honglei.li@molbase.com>
2019-07-24 13:57:45 +0800

    修改gulp的配置文件

--- food/gulpfile.js
+++ food/gulpfile.js
@@ -1,4 +1,6 @@
 let gulp = require('gulp')
+
+let del = require('del');
 // 获取js的连接，和压缩
 let concat = require('gulp-concat')
 let uglify = require('gulp-uglify')
@@ -6,9 +8,39 @@ let uglify = require('gulp-uglify')
 let concatCss = require('gulp-concat-css')
 let uglifyCss = require('gulp-uglifycss')
 
-
-
-
-gulp.task('build', function() {
-
+// 清空最终的js
+gulp.task('clean-js', function (cb) {
+    del([
+      'temp/main.min.js'
+    ], cb);
 });
+// 清空最终的css
+gulp.task('clean-css', function (cb) {
+    del([
+      'temp/main.min.css'
+    ], cb);
+});
+// 处理js，合并，检查，压缩
+gulp.task('build-js', function() {
+    gulp.src([
+        'dev/js/public.js',
+        'dev/js/index.js',
+    ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('temp'))
+});
+
+// 处理css，合并，检查，压缩
+gulp.task('build-css', function() {
+    gulp.src([
+        'dev/css/public.css',
+        'dev/css/index.css',
+    ])
+    .pipe(concatCss('main.min.css'))
+    .pipe(uglifyCss())
+    .pipe(gulp.dest('temp'))
+});
+
+// 最终build
+gulp.task('build-all', ['clean-js','clean-css','build-js', 'build-css',],function() {});
