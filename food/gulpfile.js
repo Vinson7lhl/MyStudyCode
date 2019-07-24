bd57c48d39e69b33449e51e607489ad590f6c893
honglei.li <honglei.li@molbase.com>
2019-07-24 11:29:40 +0800

    提交

--- food/gulpfile.js
+++ food/gulpfile.js
@@ -1,1 +1,14 @@
-let gulp = require('gulp');
+let gulp = require('gulp')
+// 获取js的连接，和压缩
+let concat = require('gulp-concat')
+let uglify = require('gulp-uglify')
+// 获取css的连接，和压缩
+let concatCss = require('gulp-concat-css')
+let uglifyCss = require('gulp-uglifycss')
+
+
+
+
+gulp.task('build', function() {
+
+});
