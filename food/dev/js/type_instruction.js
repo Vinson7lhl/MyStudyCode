1687ba2a2e89f217f438afdb3c9ab5e5e3dc6f10
honglei.li <honglei.li@molbase.com>
2019-07-31 16:11:54 +0800

    提交

---
+++ food/dev/js/type_instruction.js
@@ -0,0 +1,11 @@
+$(function() {
+    console.log('产品类别详情页初始化完毕')
+    let type_str = getUrlParam('type')
+    if (type_str === 'mn') {
+        $('.mn').addClass('show')
+        $('.typeName').text('泌尿道')
+    } else if(type_str === 'pf') {
+        $('.pf').addClass('show')
+        $('.typeName').text('皮肤')
+    }
+})
