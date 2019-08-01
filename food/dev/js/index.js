bbb577d1ba2c9097661dccab7aec8c26a4b476b1
honglei.li <honglei.li@molbase.com>
2019-08-01 16:19:22 +0800

    增加swiper

--- food/dev/js/index.js
+++ food/dev/js/index.js
@@ -1,3 +1,20 @@
-$(function() {
+$(function () {
     console.log('首页js初始化完毕')
+    let swiper = new Swiper('.bannerArea', {
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        loop: true, // 循环
+        autoplay: true, // 自动播放3s切换
+    })
+    let swiper2 = new Swiper('.banner2Area', {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true // 循环
+    });
 })
