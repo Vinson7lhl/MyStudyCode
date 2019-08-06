8a940d72a6386491ad57dec19e43da2fe2c87556
Seven Li-后会有七 <vinson7lhl@126.com>
2019-08-07 06:31:00 +0800

    提交

--- food/dev/js/type_instruction.js
+++ food/dev/js/type_instruction.js
@@ -4,8 +4,10 @@ $(function() {
     if (type_str === 'mn') {
         $('.mn').addClass('show')
         $('.typeName').text('泌尿道')
+        $('title').text('泌尿道')
     } else if(type_str === 'pf') {
         $('.pf').addClass('show')
         $('.typeName').text('皮肤')
+        $('title').text('皮肤')
     }
 })
