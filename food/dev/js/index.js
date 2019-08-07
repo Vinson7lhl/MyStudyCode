f29e7914c3100ee7689b2777559d5c5a488a43af
Seven Li-后会有七 <vinson7lhl@126.com>
2019-08-07 22:00:19 +0800

    提交

--- food/dev/js/index.js
+++ food/dev/js/index.js
@@ -18,9 +18,25 @@ $(function () {
         loop: true, // 循环
         on: {
             slideChangeTransitionEnd: function () {
-                $('.currentBookName').text($('.swiper-slide-active').find('.bookName').text())
-                $('.currentBookDes').text($('.swiper-slide-active').find('.bookDes').text())
+                // $('.currentBookName').text($('.swiper-slide-active').find('.bookName').text())
+                // $('.currentBookDes').text($('.swiper-slide-active').find('.bookDes').text())
                 //切换结束时，告诉我现在是第几个slide
+                if($('#banner_area2 .swiper-slide-active').attr('data-num') ==  1) {
+                    $('.currentBookNameDes').attr('src','./dev/img/index_book_des01.png')
+                    console.log('1')
+                }else if($('#banner_area2 .swiper-slide-active').attr('data-num') ==  2) {
+                    $('.currentBookNameDes').attr('src','./dev/img/index_book_des01.png')
+                    console.log('2')
+                }else if($('#banner_area2 .swiper-slide-active').attr('data-num') ==  3) {
+                    $('.currentBookNameDes').attr('src','./dev/img/index_book_des01.png')
+                    console.log('3')
+                }else if($('#banner_area2 .swiper-slide-active').attr('data-num') ==  4) {
+                    $('.currentBookNameDes').attr('src','./dev/img/index_book_des01.png')
+                    console.log('4')
+                }else if($('#banner_area2 .swiper-slide-active').attr('data-num') ==  5) {
+                    $('.currentBookNameDes').attr('src','./dev/img/index_book_des01.png')
+                    console.log('5')
+                }
             },
         },
     });
