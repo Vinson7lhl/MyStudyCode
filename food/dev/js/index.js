13bbd8c75ec202cb9169c29a500448ff2b8de3e3
honglei.li <honglei.li@molbase.com>
2019-08-07 10:20:41 +0800

    压缩图片优化代码

--- food/dev/js/index.js
+++ food/dev/js/index.js
@@ -15,6 +15,13 @@ $(function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        loop: true // 循环
+        loop: true, // 循环
+        on: {
+            slideChangeTransitionEnd: function () {
+                $('.currentBookName').text($('.swiper-slide-active').find('.bookName').text())
+                $('.currentBookDes').text($('.swiper-slide-active').find('.bookDes').text())
+                //切换结束时，告诉我现在是第几个slide
+            },
+        },
     });
 })
