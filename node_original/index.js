b462d2744daad7b40ed0c3f34edf63beeada42d1
Seven Li-后会有七 <vinson7lhl@126.com>
2020-02-15 21:30:43 +0800

    提交

--- node_original/index.js
+++ node_original/index.js
@@ -1,6 +1,6 @@
-// 文件类
+// file system文件系统对象
 const fs = require('fs')
-// 服务器类
+// http对象
 const http = require('http')
 
 // 读取写入文件
@@ -16,4 +16,15 @@ fs.writeFile ('./test_write_file.txt', '写入Halo node-express', (err)=> {
     }
 })
 
-// 服务器
+// 创建服务器对象
+const server = http.createServer()
+server.on('request', (req, res)=> {
+    console.log('请求来了！')
+    // res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.write('请求返回Hello world')
+    res.end()
+})
+// 启动服务
+server.listen(3000, ()=> {
+    console.log('启动服务')
+})
