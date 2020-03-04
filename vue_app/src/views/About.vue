977557c56b586dfc7b81731ac308b2bf43e00fd5
Seven Li-后会有七 <vinson7lhl@126.com>
2020-03-04 22:53:19 +0800

    提交
    测试全局set方法

--- vue_app/src/views/About.vue
+++ vue_app/src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 @click='changeConent'>This is an about page</h1>
+    <div>
+      {{content}}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'about-page',
+  data () {
+    return {
+      content: ['你好', '啊——', '李银河']
+    }
+  },
+  methods: {
+    changeConent () {
+      // this.content[2] = '王小波'
+      this.$set(this.content, 2, '王小波')
+    }
+  }
+}
+</script>
